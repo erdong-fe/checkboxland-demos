1c0005f3728cf766d452a7a127627731f9ecb791
erdong-fe <zhiguangphoenix@gmail.com>
2021-10-28 11:14:14 +0800

    feat: 绘制游戏区域和snake

--- src/demos/snake/index.js
+++ src/demos/snake/index.js
@@ -1,8 +1,40 @@
+import { Checkboxland } from 'checkboxland';
+import { useEffect } from 'react';
 
 const Snake = () => {
+    let snakeDemo;
+    let snake;
+
+    // 初始化游戏相关的数据
+    const _initGame = () => {
+        snakeDemo = new Checkboxland({
+            dimensions: '44x15',
+            selector: '#snake-demo'
+        });
+        snake = [{ x:2, y:7 }, { x:1, y:7 }, { x:0, y:7 }];
+        _draw();
+    }
+    
+    const _draw = () => {
+        const emptyMap = snakeDemo.getEmptyMatrix();
+
+        // 画snake
+        snake.forEach(item => {
+            emptyMap[item.y][item.x] = 1;
+        })
+
+        snakeDemo.setData(emptyMap);
+    }
+
+    useEffect(
+        () => {
+            _initGame();
+        }
+    )
+
     return (
-        <div className="snake">
-            贪吃蛇
+        <div id="snake-demo">
+
         </div>
     )
 };
