0c67965e840311f40177d953ec45412a8db66302
erdong-fe <zhiguangphoenix@gmail.com>
2021-10-31 13:48:05 +0800

    feat: 让贪吃蛇改变方向

--- src/demos/snake/index.js
+++ src/demos/snake/index.js
@@ -1,11 +1,19 @@
 import { Checkboxland } from 'checkboxland';
 import { useEffect } from 'react';
 
+const directionMap = {
+    'left': '37',
+    'right': '39',
+    'up': '38',
+    'down': '40'
+};
+
 const Snake = () => {
     let snakeDemo;
     let snake;
     let interval;
     let speed = 200;
+    let direction = directionMap.right; // 初始化默认向右
 
     // 初始化游戏相关的数据
     const _initGame = () => {
@@ -14,6 +22,10 @@ const Snake = () => {
             selector: '#snake-demo'
         });
         snake = [{ x:0, y:7 }, { x:1, y:7 }, { x:2, y:7 }];
+
+        document.querySelector('body')
+        .addEventListener('keydown', _onChangeDirection);
+
         interval = setInterval(_moveSnake, speed);
     }
 
@@ -29,10 +41,43 @@ const Snake = () => {
 
     // 移动snake
     const _moveSnake = () => {
-        for(let i = 0; i < snake.length; i++) {
-            snake[i].x++;
+        // 调整除过蛇头以外的节点
+        for(let i = 0; i < snake.length - 1; i++) {
+            snake[i] = Object.assign({}, snake[i+1]);
         }
+
+        // 调整蛇头节点
+        const snakeHead = snake[snake.length-1];
+        switch (direction) {
+            case directionMap.up:
+                snakeHead.y--;
+                break;
+            case directionMap.right:
+                snakeHead.x++;
+                break;
+            case directionMap.down:
+                snakeHead.y++;
+                break;
+            case directionMap.left:
+                snakeHead.x--;
+                break;
+            default:
+                break;
+        }
+        
         _draw();
+    }
+
+    // 响应键盘输入改变方向
+    const _onChangeDirection = (e) => {
+        const newCodeKey = e.keyCode.toString();
+        if ((newCodeKey === directionMap.left && direction === directionMap.right)
+            || (newCodeKey === directionMap.right && direction === directionMap.left)
+            || (newCodeKey === directionMap.up && direction === directionMap.down)
+            || (newCodeKey === directionMap.down && direction === directionMap.up)) {
+                return;
+        }
+        direction = newCodeKey;
     }
 
     useEffect(
