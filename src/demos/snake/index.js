936365a0ba6b43a14e98e93a5d0c23386f811871
erdong-fe <zhiguangphoenix@gmail.com>
2021-10-31 12:12:44 +0800

    feat: 让贪吃蛇前进起来

--- src/demos/snake/index.js
+++ src/demos/snake/index.js
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 const Snake = () => {
     let snakeDemo;
     let snake;
+    let interval;
+    let speed = 200;
 
     // 初始化游戏相关的数据
     const _initGame = () => {
@@ -11,19 +13,26 @@ const Snake = () => {
             dimensions: '44x15',
             selector: '#snake-demo'
         });
-        snake = [{ x:2, y:7 }, { x:1, y:7 }, { x:0, y:7 }];
-        _draw();
+        snake = [{ x:0, y:7 }, { x:1, y:7 }, { x:2, y:7 }];
+        interval = setInterval(_moveSnake, speed);
     }
-    
+
+    // 绘制
     const _draw = () => {
         const emptyMap = snakeDemo.getEmptyMatrix();
-
         // 画snake
         snake.forEach(item => {
             emptyMap[item.y][item.x] = 1;
         })
-
         snakeDemo.setData(emptyMap);
+    }
+
+    // 移动snake
+    const _moveSnake = () => {
+        for(let i = 0; i < snake.length; i++) {
+            snake[i].x++;
+        }
+        _draw();
     }
 
     useEffect(
@@ -33,9 +42,7 @@ const Snake = () => {
     )
 
     return (
-        <div id="snake-demo">
-
-        </div>
+        <div id="snake-demo" />
     )
 };
 
